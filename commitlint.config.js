9d31d8df20720d01f3ffb6e54515e8c13129dc15
bernardorz <bernardorizzatti01@gmail.com>
2023-04-17 20:07:27 -0300

    fix: Remove container dependencie

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+};
